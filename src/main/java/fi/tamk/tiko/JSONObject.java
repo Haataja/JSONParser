707f01ab2a9b34a84a3fb696989b566c1c5a5f77
Hanna Haataja <hanna.haataja@cs.tamk.fi>
2018-11-04 22:13:08 +0200

    toJsonString now works.

--- src/main/java/fi/tamk/tiko/JSONObject.java
+++ src/main/java/fi/tamk/tiko/JSONObject.java
@@ -4,35 +4,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class JSONObject {
-    private Map<String,Object> json;
+    private Map<String, Object> json;
 
-    public JSONObject(){
+    public JSONObject() {
         json = new HashMap<String, Object>();
     }
 
-    public void put(String key, Object value){
-        json.put(key,value);
+    public void put(String key, Object value) {
+        json.put(key, value);
     }
 
-    public Object get(String key){
+    public Object get(String key) {
         return json.get(key);
     }
 
-    public String toString(){
-        String string = "{";
+    @Override
+    public String toString() {
+        String string = "{\n";
         int i = 0;
-        for (String key: json.keySet()){
-            if(json.get(key) instanceof JSONArray){
-                string += "\""+key+"\":["+((JSONArray)json.get(key)).toJsonString();
-            }else if(json.get(key) instanceof JSONObject){
-                string += "\""+key+"\":"+((JSONObject) json.get(key)).toJsonString();
-            } else if (json.get(key) instanceof String){
-                string += "\""+key+"\":\""+json.get(key)+"\"";
-            }else {
-                string += "\""+key+"\":"+json.get(key);
+        for (String key : json.keySet()) {
+            if (json.get(key) instanceof JSONArray) {
+                string += "\"" + key + "\":[" + json.get(key).toString();
+            } else if (json.get(key) instanceof JSONObject) {
+                string += "\"" + key + "\":" + json.get(key).toString();
+            } else if (json.get(key) instanceof String) {
+                string += "\"" + key + "\":\"" + json.get(key) + "\"";
+            } else {
+                string += "\"" + key + "\":" + json.get(key);
             }
-            if(i < json.keySet().size() - 1){
-                string += ",";
+            if (i < json.keySet().size() - 1) {
+                string += ",\n";
+            } else {
+                string += "\n";
             }
             i++;
         }
@@ -40,10 +43,28 @@ public class JSONObject {
         return string;
     }
 
-    public String toJsonString(){
-        String JSONStirng = toString();
+    public String toJsonString() {
+        String[] JSONString = toString().split("\n");
+        String returned = "";
         String indent = "  ";
         int indentNumber = 0;
-        return JSONStirng;
+
+        for (int j = 0; j < JSONString.length; j++) {
+            if (JSONString[j].contains("}") || JSONString[j].contains("]")) {
+                indentNumber--;
+            }
+            String intended = "";
+            for (int i = 0; i < indentNumber; i++) {
+                intended += indent;
+            }
+            JSONString[j] = intended + JSONString[j];
+            if (JSONString[j].contains("{") || JSONString[j].contains("[")) {
+                indentNumber++;
+            }
+        }
+        for (String string : JSONString) {
+            returned += string + "\n";
+        }
+        return returned;
     }
 }
